2d6bd1a057b7df9dac65e416f4dcfc6e0b44c9f7
SampaioDevHub <vinicius@dhouses.com.br>
2025-02-02 14:25:58 -0300

    feat: rename Logout component to Page and adjust export syntax

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,9 @@
-'use client'; 
-
+'use client'
 import { useAuth } from '@clerk/nextjs';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
-export function Logout () {
+export default function Page () {
     const { isLoaded, userId } = useAuth();
     const router = useRouter();
 
