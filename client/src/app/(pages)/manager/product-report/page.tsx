62c0f16dd68ef1d3ee2bb857b7845597162c970d
Gabriel <gabertrol@gmail.com>
2025-02-12 12:34:41 -0300

    feat: updated in relatorio

--- client/src/app/(pages)/manager/product-report/page.tsx
+++ client/src/app/(pages)/manager/product-report/page.tsx
@@ -1,33 +1,23 @@
 /* eslint-disable import/no-unresolved */
-import GenerateStoreReport from '@/app/(pages)/manager/product-report/_components/GenerateStoreReport';
-import relatorioGif from '@/assets/relatoriogif.gif';
-import relatorioGifTab from '@/assets/relatoriotablet.jpg';
-import Image from 'next/image';
+
+import { GenerateStoreReport } from './_components/GenerateStoreReport';
 
 export default function ReportsPage() {
   return (
-    <main className='relative min-h-screen w-full items-center overflow-hidden'>
-      <div className='relative z-10'>
-        <GenerateStoreReport />
-      </div>
-      <div className='flex items-center justify-center'>
-        {/* Desktop Image */}
-        <Image
-          src={relatorioGif}
-          alt='Relatório'
-          className='hidden w-auto max-w-[80%] object-cover p-10 pt-0 lg:block'
-          sizes='(min-width: 1024px) 80vw'
-          priority
-        />
-        {/* Tablet Image */}
-        <Image
-          src={relatorioGifTab}
-          alt='Relatório'
-          className='mt-10 w-[500px] items-center object-cover pt-4 lg:hidden'
-          sizes='(max-width: 1023px) 80vw'
-          priority
-        />
-      </div>
+    <main className='container flex flex-col lg:gap-0 gap-8 px-4 py-8'>
+      <GenerateStoreReport />
+      <div
+        className='hidden h-[65vh] w-full bg-cover bg-center lg:flex'
+        style={{
+          backgroundImage: "url('/assets/relatorio-products-report.gif')"
+        }}
+      ></div>
+      <div
+        className='h-[40vh] w-full bg-cover bg-center lg:hidden'
+        style={{
+          backgroundImage: "url('/assets/relatorio-products-gif-tab.png')"
+        }}
+      ></div>
     </main>
   );
 }
