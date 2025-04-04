d62b6ae030f3170d06189ae4eaa59afa89bef65e
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-03 21:16:30 -0300

    test: add comments and refact to better reading

--- src/_components/Inputs/tests/cpfInput.test.tsx
+++ src/_components/Inputs/tests/cpfInput.test.tsx
@@ -2,6 +2,7 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import { CpfInput } from '../cpfInput';
 
 describe('CPF Input Tests', () => {
+  // mock da função onChange()
   const onChange_mock = jest.fn();
   beforeEach(() => {
     onChange_mock.mockClear();
@@ -10,31 +11,41 @@ describe('CPF Input Tests', () => {
   it('should render the input ', () => {
     // renderizo o input
     render(<CpfInput onChange={onChange_mock} value="" />);
-    const input = screen.getByRole('textbox');
 
+    // busca o input
+    const input = screen.getByRole('textbox') as HTMLInputElement;
+
+    // verifica se  o input esta sendo renderizado corretamente
     expect(input).toBeInTheDocument();
   });
 
-  it('should type the cpf on input', () => {
+  it('should type the cpf on input and call the format function', () => {
     // renderizo o input
     render(<CpfInput onChange={onChange_mock} value="" />);
+
+    // busca o input
     const input = screen.getByRole('textbox') as HTMLInputElement;
 
+    // simula a digitação de um cpf (sem formatação) no input
     fireEvent.change(input, { target: { value: '12345678909' } });
 
+    // verifica se onChange() foi chamada
     expect(onChange_mock).toHaveBeenCalled();
   });
 
-  it('should type on the input and format the value', () => {
-    // renderizo o input
+  it('should type on the input and verify if the format function is called and if the value was really formatted', () => {
+    // renderiza o input
     render(<CpfInput onChange={onChange_mock} value="" />);
+
+    // busca  o input
     const input = screen.getByRole('textbox') as HTMLInputElement;
 
-    // simula a digitação de um cep (sem formatação) no input
+    // simula a digitação de um cpf (sem formatação) no input
     fireEvent.change(input, { target: { value: '12345678909' } });
 
     // verifica se onChange() foi chamada e se formatou o input
     expect(onChange_mock).toHaveBeenCalledWith(
+      // espera que o valor no  retorno seja o cpf formatado
       expect.objectContaining({
         target: expect.objectContaining({
           value: '123.456.789-09',
@@ -42,15 +53,15 @@ describe('CPF Input Tests', () => {
       })
     );
 
-    // renderiza novamente
+    // renderiza novamente o input
     render(<CpfInput onChange={onChange_mock} value="123.456.789-09" />);
 
-    // outra renderização do input porem com o cep digitado formatado
+    // outra renderização do input porem com o cpf digitado formatado
     const inputFormatted = screen.getByDisplayValue(
       '123.456.789-09'
     ) as HTMLInputElement;
 
-    //verificando se exibe o cep formatado
+    //verificando se exibe o cpf formatado
     expect(inputFormatted.value).toBe('123.456.789-09');
   });
 });
