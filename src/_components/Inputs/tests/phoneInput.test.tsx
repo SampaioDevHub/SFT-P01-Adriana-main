231dcab120a7e5fe7f2ac0dad13b08fef92d6467
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-04-07 18:12:50 -0300

    test: improve brazillian and international numbers formatting tests

--- src/_components/Inputs/tests/phoneInput.test.tsx
+++ src/_components/Inputs/tests/phoneInput.test.tsx
@@ -38,7 +38,7 @@ describe('Phone Input Tests', () => {
                 }}
                 value={test_value}
                 defaultCountry="BR"
-                international={false}
+                international={false} // numero vai conter () e - na formatação
             />
         );
 
@@ -53,6 +53,8 @@ describe('Phone Input Tests', () => {
 
         // verifica se em tela é renderizado de fato o numero 100% formatado
         expect(input.value).toBe('(21) 99999-9999');
+        /* 
+        após pesquisa verifiquei que números nacionais na lib react-phone-number-input  usam () e - ao formatar com a prop international={false} */
     });
     it('should  type on the input with a international (US) phone number value and verify if the onChange is called (the value must be formatted)', async () => {
         // inicio o valor do input vazio
@@ -66,6 +68,7 @@ describe('Phone Input Tests', () => {
                 }}
                 value={test_value}
                 defaultCountry="US"
+                international={false} // numero vai conter () e - na formatação
             />
         );
 
@@ -73,12 +76,15 @@ describe('Phone Input Tests', () => {
         const input = screen.getByRole('textbox') as HTMLInputElement;
 
         // simula a digitação/preenchimento do input com um numero internacional sem formatação
-        await userEvent.type(input, '16202134144');
+        await userEvent.type(input, '6202134144');
 
         // verifica se o value agora tem formatação no formato E.164
         expect(test_value).toBe('+16202134144');
 
         // verifica se em tela é renderizado de fato o numero 100% formatado
-        expect(input.value).toBe('1 (620) 213-4144');
+        expect(input.value).toBe('(620) 213-4144'); /* 
+        após pesquisa verifiquei que números internacionais na lib react-phone-number-input não usam () e nem - ao formatar com a prop international={true} então é necessário neste caso de teste international={false} para poder entrar na formatação do site mesmo sendo internacional. 
+        é interessante manter international={false} para a UI/UX (foco maior na UX) seguir o padrão, mesmo em números internacionais.
+         */
     });
 });
